55d4df33e8120dc6c06e02a3e2430b234f3f9c8b
TeyaDavis23 <teyadav@unc.edu>
2023-12-14 13:41:02 -0500

    Modified project clustering script to save images to figures directory.

--- Project_Clustering.R
+++ Project_Clustering.R
@@ -11,6 +11,10 @@ forPCA <- ForbesRichestAthletes %>%
 results <- prcomp(forPCA, center = TRUE, scale = TRUE)
  plot(results, type = "l")
 
+dev.copy(png,'/home/rstudio/work/figures/PCA1.png')
+dev.off()
+ 
+
 #From the plot of variances as a function of the number of components, we can
 #see that most of the variance in the data is explained by the first two 
 #principal components.
@@ -25,7 +29,20 @@ plotData <- plotData %>%
          PC2 = as.numeric(PC2))
 
 ggplot(plotData %>% as_tibble(), aes(PC1,PC2)) + 
-  geom_point(aes(color = new_Sport))
+  geom_point()
+
+ggsave("PCA2.png", path = "/home/rstudio/work/figures", 
+       width = 5, height = 3, )
+
+ggplot(plotData %>% as_tibble(), aes(PC1,PC2)) + 
+  geom_point(aes(color = new_Sport)) +
+  guides(color = guide_legend(title = "Sport")) +
+  theme(legend.text = element_text(size=5),
+        legend.key.size = unit(0.35, 'cm'),
+        legend.title = element_text(size=8))
+
+ggsave("PCA3.png", path = "/home/rstudio/work/figures", 
+       width = 5, height = 3, )
 
 #From the scatter plot of the first and second components, it does not look like
 # there are any significant clusters in the data. Therefore, the t-SNE projection
