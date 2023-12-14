6fe27297c7fc9ca925b2cee2e25960fcb7ccf45e
TeyaDavis23 <teyadav@unc.edu>
2023-12-14 12:56:54 -0500

    Edited figure 4 (title and legend).

--- Project_Figure4.R
+++ Project_Figure4.R
@@ -7,10 +7,13 @@ Sport_Earnings <- read_csv("derived_data/Sport_Earnings.csv")
 ggplot(data = Sport_Earnings, aes(x = Year, y = total_earnings)) +
   geom_point(aes(color = new_Sport)) +
   xlab("Year") +
-  ylab("Combined Earnings (in millions of dollars)") +
-  ggtitle("Scatterplot of Combined Earnings by Sport vs. Year for \n Forbes' Richest Athletes") +
+  ylab("Combined Earnings \n (in millions of dollars)") +
+  ggtitle("Scatterplot of \n Combined Earnings by Sport vs. Year") +
   theme(plot.title = element_text(hjust = 0.5)) +
-  guides(color = guide_legend(title = "Sport"))
+  guides(color = guide_legend(title = "Sport")) +
+  theme(legend.text = element_text(size=5),
+        legend.key.size = unit(0.35, 'cm'),
+        legend.title = element_text(size=8))
 
 ggsave("Scatterplot1204.png", path ="/home/rstudio/work/figures", 
        width = 5, height = 3)
